798b3b2be9531eb4f1cabbc6022cc38e0b51fe65
Eduardo Cunha Ribeiro <eduzinhobrp@gmail.com>
2026-03-24 10:54:00 -0300

    Agora sim: apenas a pasta POO no repositório

---
+++ POO/Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis {
+    public static void main(String[] args){
+        String personagem = "Homem-Aranha";
+        int cartuchoDeTeia = 18;
+        double precisaoDeLente = 99.9;
+        booblean mascaraEquipada = true;
+        System.out.println("Personagem:" + personagem);
+        
+    }
+    
+}
